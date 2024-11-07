b36ec26512d80ebd6f1e8f1f77996b4f4d6261ba
Hazedd <hazzdd@no-replay.github.com>
2024-11-07 08:48:04 +0100

    Bump version: 0.2.0-dev5 → 0.2.0-dev6

--- imxInsights/__init__.py
+++ imxInsights/__init__.py
@@ -2,7 +2,7 @@ from imxInsights.file.containerizedImx.imxContainer import ImxContainer
 from imxInsights.file.singleFileImx.imxSingleFile import ImxSingleFile
 from imxInsights.repo.imxMultiRepo import ImxMultiRepo
 
-__version__ = "0.2.0-dev5"
+__version__ = "0.2.0-dev6"
 
 __all__ = [
     "ImxContainer",
