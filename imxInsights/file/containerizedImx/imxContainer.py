3a8e340a68a9a59dc1031d5dcee4e75047072de3
Hazedd <hazzdd@no-replay.github.com>
2024-11-17 23:21:24 +0100

    :recycle: methode made static

--- imxInsights/file/containerizedImx/imxContainer.py
+++ imxInsights/file/containerizedImx/imxContainer.py
@@ -35,7 +35,8 @@ class ImxContainer(ImxRepo):
         logger.success(f"Finished processing {self._input_file_path.name}")
         self.dataframes = ContainerImxPandasGenerator(self)
 
-    def _initialize_file_path(self, imx_file_path: Path | str) -> Path:
+    @staticmethod
+    def _initialize_file_path(imx_file_path: Path | str) -> Path:
         """Initialize and return a Path object from the input file path."""
         logger.info(f"Processing {Path(imx_file_path).name}")
         return Path(imx_file_path) if isinstance(imx_file_path, str) else imx_file_path
