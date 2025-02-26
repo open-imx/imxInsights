a627363e0edc2ad73b4ce530a540383dab58deec
Hazedd <hazzdd@no-replay.github.com>
2025-02-26 09:31:14 +0100

    raise errors on missing measures

--- imxInsights/repo/builders/buildOnRailConnectionGeometry.py
+++ imxInsights/repo/builders/buildOnRailConnectionGeometry.py
@@ -6,7 +6,6 @@ from lxml.etree import _Element as Element
 
 from imxInsights.domain.imxObject import ImxObject
 from imxInsights.domain.onRailConnectionGeometry import (
-    RailConnectionInfoException,
     RailConnectionInfoFailed,
     RailConnectionPoint,
     RailConnectionProfile,
@@ -41,19 +40,23 @@ def process_on_rail_connection_geometry(
             return
 
         at_measure = rail_con_info.get("atMeasure", None)
-        try:
-            if at_measure:
-                imx_object.on_rail_geometry.append(
-                    RailConnectionPoint(rail_con_info, rail_con)
-                )
-            else:
-                imx_object.on_rail_geometry.append(
-                    RailConnectionProfile(rail_con_info, rail_con)
-                )
+        from_measure = rail_con_info.get("fromMeasure", None)
+        to_measure = rail_con_info.get("toMeasure", None)
 
-        except RailConnectionInfoException as e:
-            logger.error(f"{imx_object.puic} ({imx_object.tag}) {e}")
-            imx_object.on_rail_geometry.append(RailConnectionInfoFailed(rail_con_info))
+        if at_measure is not None and (from_measure is not None or to_measure is not None):
+            raise ValueError("If 'atMeasure' is present, 'fromMeasure' and 'toMeasure' must not be present.")
+
+        if at_measure is None and (from_measure is None or to_measure is None):
+            raise ValueError("If 'atMeasure' is not present, both 'fromMeasure' and 'toMeasure' must be present.")
+
+        if at_measure:
+            imx_object.on_rail_geometry.append(
+                RailConnectionPoint(rail_con_info, rail_con)
+            )
+        else:
+            imx_object.on_rail_geometry.append(
+                RailConnectionProfile(rail_con_info, rail_con)
+            )
 
 
 def create_rail_connection_info_geometry(
@@ -62,9 +65,12 @@ def create_rail_connection_info_geometry(
 ) -> None:
     for key, imx_objects in tree_dict.items():
         for imx_object in imx_objects:
-            process_on_rail_connection_geometry(
-                imx_object, find, "{http://www.prorail.nl/IMSpoor}RailConnectionInfo"
-            )
+            try:
+                process_on_rail_connection_geometry(
+                    imx_object, find, "{http://www.prorail.nl/IMSpoor}RailConnectionInfo"
+                )
+            except ValueError as e:
+                logger.error(f"{imx_object.puic} ({imx_object.tag}) cant create railConnectionInfo geometry {e}")
 
 
 def create_track_fragment_geometry(
@@ -73,9 +79,12 @@ def create_track_fragment_geometry(
 ) -> None:
     for key, imx_objects in tree_dict.items():
         for imx_object in imx_objects:
-            process_on_rail_connection_geometry(
-                imx_object, find, "{http://www.prorail.nl/IMSpoor}TrackFragment"
-            )
+            try:
+                process_on_rail_connection_geometry(
+                    imx_object, find, "{http://www.prorail.nl/IMSpoor}TrackFragment"
+                )
+            except ValueError as e:
+                logger.error(f"{imx_object.puic} ({imx_object.tag}) cant create TrackFragment geometry {e}")
 
 
 def create_demarcation_geometry(
@@ -84,9 +93,12 @@ def create_demarcation_geometry(
 ) -> None:
     for key, imx_objects in tree_dict.items():
         for imx_object in imx_objects:
-            process_on_rail_connection_geometry(
-                imx_object, find, "{http://www.prorail.nl/IMSpoor}DemarcationMarker"
-            )
+            try:
+                process_on_rail_connection_geometry(
+                    imx_object, find, "{http://www.prorail.nl/IMSpoor}DemarcationMarker"
+                )
+            except ValueError as e:
+                logger.error(f"{imx_object.puic} ({imx_object.tag}) cant create DemarcationMarker geometry {e}")
 
 
 def add_on_rail_connection_geometry(
