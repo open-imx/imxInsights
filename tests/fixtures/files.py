540fd549b1c075f532f35fca40aea91583316126
Hazedd <hazzdd@no-replay.github.com>
2025-04-08 23:13:51 +0200

    restructured, add typehints and future features todos

--- tests/fixtures/files.py
+++ tests/fixtures/files.py
@@ -1,3 +1,5 @@
+import json
+
 import pytest
 
 from tests.helpers import sample_path
@@ -21,3 +23,10 @@ def imx_v1200_test_zip_file_path() -> str:
 @pytest.fixture(scope="module")
 def imx_v1200_test_dir_file_path() -> str:
     return sample_path("1200/set_1")
+
+
+@pytest.fixture(scope="module")
+def load_test_features() -> dict:
+    with open(sample_path("measure_test_set.geojson"), "r") as f:
+        geojson = json.load(f)
+    return geojson["features"]
