b6e7242c01492a5951ee4a707eefa5001d2122cb
Hazedd <20663495+Hazedd@users.noreply.github.com>
2025-07-03 10:02:33 +0200

    lastest from azure repo

--- imxInsights/compare/headerLoader.py
+++ imxInsights/compare/headerLoader.py
@@ -18,57 +18,30 @@ class HeaderLoader:
             if col + "_link" in self.spec.columns:
                 self.spec[col] = '=HYPERLINK("'+self.spec[col + "_link"]+'", "'+self.spec[col]+'")'
                 self.spec = self.spec.drop([col + "_link"], axis="columns")
+
+        self.spec = self.spec.drop(
+            ignore, axis="columns", errors="ignore"
+        ).drop_duplicates()
         self.pathfield = pathfield
-        self.ignore = ignore
 
-    def _info_block_for_path(self, path):
-        """
-        Retrieves the specification information block for a given object path.
-
-        Args:
-            path (str): The object path for which to retrieve the specification information.
-
-        Returns:
-            pandas.DataFrame | None: A transposed DataFrame containing the specification
-            values for the given path if exactly one match is found. Returns None if
-            no match or multiple matches are found.
-        """
-        # Remove the Diff information
-        if "->" in path:
+    @staticmethod
+    def remove_extra_path(s):
+        if "->" in s:
             # This happens for paths that changed, for example InitialSituation->newsituation
             # We use the new situation
-            path = path.partition("->")[2].strip()
+            s = path.partition("->")[2].strip()
 
-        if path.startswith("++") or path.startswith("--"):
-            path = path[2:].strip()
+        if s.startswith("++") or s.startswith("--"):
+            s = s[2:].strip()
 
-        if path.lower().startswith("imspoor"):
-            path = ".".join(path.split(".")[1:])
+        if s.lower().startswith("imspoor"):
+            s = ".".join(s.split(".")[1:])
 
-        if path.startswith(".extension"):
-            path = path[1:].partition(".")[-1]
-            matching_rows = self.spec[
-                self.spec[self.pathfield].str.endswith("." + path, na=False)
-            ]
-        else:
-            matching_rows = self.spec[self.spec[self.pathfield] == path]
+        return s
 
-        # drop the path selecter, and remove any exact duplicates
-        # The order is important here, as some paths might case double matches (specificly extentions) but if the information is exactly the same this does not matter
-        matching_rows = matching_rows.drop(
-            [self.pathfield] + list(self.ignore), axis="columns", errors="ignore"
-        ).drop_duplicates()
-
-        if matching_rows.empty:
-            logger.error(f"No specification found for field with path {path}")
-            return None
-        elif len(matching_rows) > 1:
-            logger.error(f"Specification not unique for field with path {path}")
-            return None
-
-        info = matching_rows.transpose()
-
-        return info
+    @staticmethod
+    def remove_numeric(s):
+        return ".".join(part for part in s.split(".") if not part.isnumeric())
 
     def add_header_to_sheet(self, df):
         """
@@ -85,96 +58,61 @@ class HeaderLoader:
         Returns:
             pandas.DataFrame: The modified DataFrame with specification header rows prepended.
         """
-        klasse = df["path_to_root"].values[0]
-        column_names = df.columns
-        # Add missing collumns
-        paths_from_spec = self.spec[self.spec[self.pathfield].str.startswith(klasse)]
-        fields_from_spec = [path[len(klasse) + 1 :] for path in paths_from_spec]
-        for field in fields_from_spec:
-            if field not in column_names:
-                df[field] = None
 
-        # We keep track of the information for all collumns to concat later
-        infoblocks = []
-        # voeg de instructies en uitwisselscope toe obv het path
-        for column in column_names:
-            if (
-                column
-                in {
-                    "path",
-                    "tag",
-                    "parent",
-                    "children",
-                    "status",
-                    "geometry_status",
-                    "path_to_root",
-                }
-                or "|" in column
-            ):
-                # These are added by the library and hence heve no info fields
-                continue
+        basepath = self.remove_extra_path(df["path_to_root"].values[0]) + "."
 
-            # columns might have numerical fields in them in case of list, e.g., Announcement.0.@announcementType.
-            # For lookup purposes we want to ignore those
-            ending = ".".join(
-                part for part in column.split(".") if not part.isnumeric()
-            )
+        # Info values that are relavent for this frame
+        rel_spec = self.spec[self.spec[self.pathfield].str.startswith(basepath)].copy()
+        rel_spec["field"] = rel_spec[self.pathfield].str.slice(start=len(basepath))
 
-            if column.startswith("extension."):
-                # We do not have the full path for extentions, matching ending will have to do
-                path = "." + ending
-            else:
-                # We should have the exact full path now
-                path = f"{klasse}.{ending}"
+        # Column names as present in the frame
+        from_frame = pd.DataFrame({"field": df.columns})
+        from_frame["path"] = (basepath + from_frame.field).map(self.remove_numeric)
 
-            info = self._info_block_for_path(path)
+        # We match on path, as this is the path without numeric parts
+        # However, we keep the field names with the numeric part if present.
+        # If a field is not present then it will be filled form the CSV file.
 
-            if info is not None:
-                # Rename the collumn so it matches the actual column name
-                numbername = info.columns[0]
-                info[column] = info[numbername]
-                info = info.drop([numbername], axis="columns")
+        info1 = pd.merge(
+            rel_spec, from_frame, how="left", left_on=self.pathfield, right_on="path"
+        )
+        info1["field"] = info1["field_y"].fillna(info1["field_x"])
+        info1.index = info1.field
+        info1 = info1.drop(
+            ["field_x", "field_y", "field", self.pathfield, "path"],
+            axis="columns",
+            errors="ignore",
+        )
 
-                infoblocks.append(info)
+        # At this point all normal fields should be populated in the info frame.
+        # We might be missing extention objects
+        # for this we check if any of the field names are present as full paths in the spec
+        # TODO test with big file
+        info2 = pd.merge(
+            from_frame, self.spec, how="inner", left_on="field", right_on=self.pathfield
+        )
+        info2.index = info2.field
+        info2 = info2.drop(
+            ["field", "path_y", "path_x", self.pathfield, "path"],
+            axis="columns",
+            errors="ignore",
+        )
 
-        # Make dataframe for the top rows of the excel
-        if infoblocks:
-            fullinfo = pd.concat(infoblocks, axis="columns").dropna(how="all")
-        else:
-            # None of the fields had any info, can happen, particulary in diff between different IMX versions
-            fullinfo = pd.DataFrame()
+        info = pd.concat([info1, info2]).transpose().dropna(how="all")
 
-        # Reformat hyperlinks as an excel formula when present
-        for i in range(1, 3):
-            if f"ref_{i}" in fullinfo.index:
-                refnames = fullinfo.loc[f"ref_{i}", :]
-                refurls = fullinfo.loc[f"ref_{i}_link", :]
-                fullinfo.loc[f"ref_{i}", :] = (
-                    '=HYPERLINK("' + refurls + '", "' + refnames + '")'
-                )
-                fullinfo.drop([f"ref_{i}_link"], inplace=True)
-
-        if "uitwisselscope_DO" in fullinfo.index:
-            fullinfo.loc["uitwisselscope_DO"] = fullinfo.loc[
-                "uitwisselscope_DO"
-            ].replace({True: "In scope", False: "Buiten scope"})
-        if "uitwisselscope_RVTO" in fullinfo.index:
-            fullinfo.loc["uitwisselscope_RVTO"] = fullinfo.loc[
-                "uitwisselscope_RVTO"
-            ].replace({True: "In scope", False: "Buiten scope"})
+        # TODO: add info for display and analyse collumns
 
         # Add the index as an actual collumn at the start
-        labels = pd.DataFrame({"OTL": fullinfo.index})
-        labels = labels.set_index(labels.OTL)
-        fullinfo = pd.concat([labels, fullinfo], axis="columns")
-
-        all_columns = set(fullinfo.columns.to_list()) | set(df.columns.to_list())
+        labels = pd.DataFrame({"Field": info.index})
+        labels = labels.set_index(labels.Field)
+        info = pd.concat([labels, info], axis="columns")
 
         # Ordering:
-        # 1. OTL
+        # 1. Fields
         # 2. Added by imxInsights != |display
         # 3. all fields in order
         # 4. all extentions
+        # 5. path to root
         def col_key(colname):
             base_fields = [
                 "@puic",
@@ -186,7 +124,7 @@ class HeaderLoader:
                 "status",
                 "geometry_status",
             ]
-            if colname == "OTL":
+            if colname == "Field":
                 return (0, colname)
             if not colname.strip():
                 return (5, 0)
@@ -200,8 +138,10 @@ class HeaderLoader:
             else:
                 return (is_extention_code, colname, 0)
 
-        in_order = sorted(list(all_columns), key=col_key)
+        all_columns = list(set(info.columns.to_list()) | set(df.columns.to_list()))
+        in_order = sorted(all_columns, key=col_key)
 
         # Hack together the order
         ordering_df = pd.DataFrame(columns=in_order)
-        return pd.concat([ordering_df, fullinfo, df])
+
+        return pd.concat([ordering_df, info, df])
